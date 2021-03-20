022cf593b8d63956fd4939ec2370dec8fb02802d
SlimVic7 <66301101+SlimVic7@users.noreply.github.com>
2021-03-21 00:23:30 +0100

    linkadd

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($) {
 
         $(window).load(function() { // makes sure the whole site is loaded
             $(".seq-preloader").fadeOut(); // will first fade out the loading animation
-            $(".sequence").delay(250).fadeOut("fast"); // will fade out the white DIV that covers the website.
+            $(".sequence").delay(200).fadeOut("fast"); // will fade out the white DIV that covers the website.
         })
       
         
